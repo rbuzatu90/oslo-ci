d8850b805638698ad9b818a1c8ea15940a8217ac
Remus Buzatu <r.buzatu90@gmail.com>
2017-03-18 00:38:00 +0200

    Get more HyperV logs
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- jobs/run_test.sh
+++ jobs/run_test.sh
@@ -52,20 +52,34 @@ PROJECT_NAME=$(basename $ZUUL_PROJECT)
 ZUUL_SITE=`echo "$ZUUL_URL" |sed 's/.\{2\}$//'`
 if [ -z "$PROJECT_NAME" ]; then echo "Could not get project name. ZUUL_PROJECT is $ZUUL_PROJECT and PROJECT_NAME is $PROJECT_NAME"; exit 1; fi
 
-run_wsman_ps $hyperv $WIN_USER $WIN_PASS 'Remove-Item -Recurse -Force C:\OpenStack\oslo-ci ; git clone https://github.com/cloudbase/oslo-ci C:\OpenStack\oslo-ci ; cd C:\OpenStack\oslo-ci ; git checkout cambridge-2016 2>&1' | tee create-environment.log
+run_wsman_ps $hyperv $WIN_USER $WIN_PASS 'Remove-Item -Recurse -Force C:\OpenStack\oslo-ci ; git clone https://github.com/cloudbase/oslo-ci C:\OpenStack\oslo-ci ; cd C:\OpenStack\oslo-ci ; git checkout cambridge-2016 2>&1' | tee create-environment-$ZUUL_UUID.log
 
 set +e
-run_wsman_cmd $hyperv $WIN_USER $WIN_PASS '"bash C:\OpenStack\oslo-ci\Hyper-V\gerrit-git-prep.sh --zuul-site '$ZUUL_SITE' --gerrit-site '$ZUUL_SITE' --zuul-ref '$ZUUL_REF' --zuul-change '$ZUUL_CHANGE' --zuul-project '$ZUUL_PROJECT' 2>&1"' | tee -a create-environment.log
-run_wsman_ps $hyperv $WIN_USER $WIN_PASS '"C:\OpenStack\oslo-ci\Hyper-V\build_and_run.ps1 -branchName '$ZUUL_BRANCH' -buildFor '$ZUUL_PROJECT' 2>&1"' | tee -a create-environment.log
+run_wsman_cmd $hyperv $WIN_USER $WIN_PASS '"bash C:\OpenStack\oslo-ci\Hyper-V\gerrit-git-prep.sh --zuul-site '$ZUUL_SITE' --gerrit-site '$ZUUL_SITE' --zuul-ref '$ZUUL_REF' --zuul-change '$ZUUL_CHANGE' --zuul-project '$ZUUL_PROJECT' 2>&1"' | tee -a create-environment-$ZUUL_UUID.log
+run_wsman_ps $hyperv $WIN_USER $WIN_PASS '"C:\OpenStack\oslo-ci\Hyper-V\build_and_run.ps1 -branchName '$ZUUL_BRANCH' -buildFor '$ZUUL_PROJECT' 2>&1"' | tee -a create-environment-$ZUUL_UUID.log
 result_run=$?
 
-run_wsman_ps $hyperv $WIN_USER $WIN_PASS "Get-Content $windows_logs_folder\unittest_output.txt" | tee unittest_output.log
-run_wsman_ps $hyperv $WIN_USER $WIN_PASS "C:\OpenStack\oslo-ci\Hyper-V\cleanup.ps1" | tee -a create-environment.log
-gzip -9 unittest_output.log
-gzip -9 create-environment.log
+run_wsman_ps $hyperv $WIN_USER $WIN_PASS "Get-Content $windows_logs_folder\unittest_output.txt" | tee unittest_output-$ZUUL_UUID.log
+run_wsman_ps $hyperv $WIN_USER $WIN_PASS "Get-Content $windows_logs_folder\subunit.out" | tee subunit-$ZUUL_UUID.log
+run_wsman_ps $hyperv $WIN_USER $WIN_PASS "Get-Content $windows_logs_folder\pip_freeze.log" | tee pip_freeze-$ZUUL_UUID.log
+run_wsman_ps $hyperv $WIN_USER $WIN_PASS "Get-Content $windows_logs_folder\results.html" | tee results-$ZUUL_UUID.html
+
+if [[ -z $IS_DEBUG_JOB ]] || [[ $IS_DEBUG_JOB == "no" ]]; then
+    run_wsman_ps $hyperv $WIN_USER $WIN_PASS "C:\OpenStack\oslo-ci\Hyper-V\cleanup.ps1" | tee -a create-environment-$ZUUL_UUID.log
+fi
+
+gzip -9 unittest_output-$ZUUL_UUID.log
+gzip -9 create-environment-$ZUUL_UUID.log
+gzip -9 pip_freeze-$ZUUL_UUID.log
+gzip -9 subunit-$ZUUL_UUID.log
+gzip -9 results-$ZUUL_UUID.html
+
 ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "if [ -z '$ZUUL_CHANGE' ] || [ -z '$ZUUL_PATCHSET' ]; then echo 'Missing parameters!'; exit 1; elif [ ! -d /srv/logs/$PROJECT_NAME/$ZUUL_CHANGE/$ZUUL_PATCHSET ]; then mkdir -p /srv/logs/$PROJECT_NAME/$ZUUL_CHANGE/$ZUUL_PATCHSET; else rm -rf /srv/logs/$PROJECT_NAME/$ZUUL_CHANGE/$ZUUL_PATCHSET/*; fi"
-scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "unittest_output.log.gz" logs@logs.openstack.tld:/srv/logs/$PROJECT_NAME/$ZUUL_CHANGE/$ZUUL_PATCHSET
-scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "create-environment.log.gz" logs@logs.openstack.tld:/srv/logs/$PROJECT_NAME/$ZUUL_CHANGE/$ZUUL_PATCHSET
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "unittest_output-$ZUUL_UUID.log.gz" logs@logs.openstack.tld:/srv/logs/$PROJECT_NAME/$ZUUL_CHANGE/$ZUUL_PATCHSET/unittest_output.log.gz
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "subunit-$ZUUL_UUID.log.gz" logs@logs.openstack.tld:/srv/logs/$PROJECT_NAME/$ZUUL_CHANGE/$ZUUL_PATCHSET/subunit.log.gz
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "pip_freeze-$ZUUL_UUID.log.gz" logs@logs.openstack.tld:/srv/logs/$PROJECT_NAME/$ZUUL_CHANGE/$ZUUL_PATCHSET/pip_freeze.log.gz
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "results-$ZUUL_UUID.html.gz" logs@logs.openstack.tld:/srv/logs/$PROJECT_NAME/$ZUUL_CHANGE/$ZUUL_PATCHSET/results.html.gz
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "create-environment-$ZUUL_UUID.log.gz" logs@logs.openstack.tld:/srv/logs/$PROJECT_NAME/$ZUUL_CHANGE/$ZUUL_PATCHSET/create-environment.log.gz
 
 set -e
 
